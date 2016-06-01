bd7d6eb2ca2ac1445e6cfe8fda9b44aa535284a2
Mobiusor <mobiusor@gmail.com>
2016-06-01 21:27:29 +0800

    Initial Commit

---
+++ BingWallpaper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BingWallpaper
+//
+//  Created by Bruno Bu on 6/1/16.
+//  Copyright © 2016 Mobiusor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BingWallpaper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BingWallpaper
+//
+//  Created by Bruno Bu on 6/1/16.
+//  Copyright © 2016 Mobiusor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
